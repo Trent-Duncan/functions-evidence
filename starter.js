e45cc2fd424a30848058e33105191dd72631dbf7
Trent-Duncan <duncantr131@northwested.org>
2026-04-27 10:25:15 -0400

    Update starter.js

--- starter.js
+++ starter.js
@@ -8,19 +8,23 @@
 
 function getHeight() {
     let height = prompt("Please enter the height of the right triangle:");
-    parseFloat(height)
+    return parseFloat(height);
 }
 
 function getBase() {
     let base = prompt("Please enter the base of the right triangle:");
-    parseFloat(base)
+    return parseFloat(base);
 }
 
-function calculateArea() {
-    let area = 0.50 * (height * base)
+function calculateArea(height, base) {
+    return 0.50 * (height * base);
 }
+let height = getHeight();
+let base = getBase();
+let area = calculateArea(height, base);
 
-alert("With a height of, " + height + "and a base of, " + base + "that gives you an area of, " + area);
+
+alert("With a height of, " + height + " and a base of, " + base + " that gives you an area of, " + area);
 
 
 
