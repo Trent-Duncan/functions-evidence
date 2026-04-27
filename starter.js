ac00ce807a9afb30f4ea7495ad82f3bb40f78792
Trent-Duncan <duncantr131@northwested.org>
2026-04-27 10:02:58 -0400

    Update starter.js

--- starter.js
+++ starter.js
@@ -6,12 +6,21 @@
 
 
 
+function getHeight() {
+    let height = prompt("Please enter the height of the right triangle:");
+    parseFloat(height)
+}
 
+function getBase() {
+    let base = prompt("Please enter the base of the right triangle:");
+    parseFloat(base)
+}
 
+function calculateArea() {
+    let area = 0.50 * (height * base)
+}
 
-
-
-
+alert("With a height of, " + height + "and a base of, " + base + "that gives you an area of, " + area);
 
 
 
